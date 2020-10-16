e96f7369208ff48385d5ea1d50426eaab989b3fe
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-10-15 22:13:33 -0700

    Big sensor update

--- README.md
+++ README.md
@@ -1,50 +1,90 @@
-# homebridge-fujitsu
-## Homebridge plug in for Fujistu Mini Split
+# homebridge-fujitsu-smart
+## Homebridge plug in for Fujistu Mini Split with smart temperature support using Xiaomi sensors.
 
 **Warning** this plugin this is currently experimental, use at your own risk!
 
 ## Installation
 
 1. Install [homebridge](https://github.com/nfarina/homebridge#installation-details)
-2. Install this plugin: `npm install -g homebridge-fujitsu`
+2. Install this plugin: `npm install -g homebridge-fujitsu-smart`
 3. Edit your `config.json` file (See below).
 
 ```json
 "accessories": [
     {
-        "accessory": "FGLairThermostat",
+        "accessory": "FGLairSmartThermostat",
         "name": "Fujitsu Mini Spit",
         "username": "FGLAIR USERNAME",
         "password": "FGLAIR PASSWORD",
-        "interval": 30
+        "interval": 30,
+        "smart": {
+            "username": "MII USERNAME",
+            "password": "MII PASSWORD",
+            "region": "cn",
+            "reference": "THERMOSTAT ROOM",
+            "feelslike": true,
+            "hold": 60,
+            "schedule": [
+                { "day": "Any", "from": "12:00am", "to": "11:59pm", "occupied":  50, "empty":  0, "room": "THERMOSTAT ROOM" },
+                { "day": "Any", "from": "12:00am", "to": "11:59pm", "occupied": 100, "empty":  0, "room": "Bedroom" }
+            ]
+        }
     },
 ]
 ```
 | Key | Description |
 | --- | --- |
-| `accessory` | Must be `FGLairThermostat` |
+| `accessory` | Must be `FGLairSmartThermostat` |
 | `name` | Name to appear in the Home app |
-| `username` | FGLair Username |
-| `password` | FGLair Password |
+| `username` | `FGLair` Username |
+| `password` | `FGLair` Password |
 | `model` _(optional)_ | Appears under "Model" for your accessory in the Home app |
 | `interval` _(optional)_ | Polling time for thermostat (Default: 60 sec.) |
 | `region` _(optional)_ | Region for thermostat, change for China & E.U. (Default: "us") |
+| `smart` _(optional)_ | Sensor configuration to allow smart adaption of thermostat temperature. |
+| `smart.username` | `Mi Home` Username |
+| `smart.password` | `Mi Home` Password |
+| `smart.region` _(optional)_ | Will default to `cn` which supports the latest range of sensor types, but can be set to other regions |
+| `smart.interval` _(optional)_ | Polling time for sensors (Default: 60 sec.) |
+| `smart.reference` | The room name of a sensor which is used as the temperature reference when making thermostat adjustments. This should be the sensor nearest the thermostat (the thermostat API doesn't provide its own temperature reading) |
+| `smart.feeslike` _(optional)_ | If `true` the temperatures will be adjusted based on the humidity, to better refect the temperatures rooms feel |
+| `smart.hold` _(optional)_ | Number of minutes (Default: 60 mins.) to hold the temperature without adjustment if changed externally (e.g. on the wall thermostat)
+| `smart.schedule` | Zero or more schedule entries describing how and when a room sensor is important |
+| `smart.schedule.day` | A day `(Sun,Mon,Tue,Wed,Thu,Fri,Sat)` or a series of days `(Mon-Thu)` or `Any` specifying which day this schedule applies |
+| `smart.schedule.from` | A time `(e.g 1:00pm,12:13am,11:59pm)` when this schedule starts |
+| `smart.schedule.to` | A time when this schedule ends |
+| `smart.schedule.occupied` | A weight from `0-100` specifying how important this schedule is, higher being more important. This weight is for when the room is occupied (or always if the room has no motion sensor) |
+| `smart.schedule.empty` | A weight for when the room has been empty for 30 minutes or more |
+| `smart.schedule.room` | The room name which should match the beginning name of the sensor `(e.g. Dining Room` has sensors `Dining Room Temperature` and `Dining Room Movement)` |
 
-##New Features
-- Added China and European servers for API, change region to
-- Improved update speed when changing modes / temps in Home App
+## Smart operation
+Smart operation uses Xiomi temperature, humidity and movement sensors to adjust the thermostat target temperature depending on where you are in your house (in a similar way to the Ecobee thermostat). Sensors must be registered using the `Mi Home` application.
+
+This plugin will read the sensors periodically, then caculate a weighted temperature based on the defined schedule, and adjust the thermostat target temperature based on this temperature and the original target temperature. HomeKit will always see the original target temperature, but the wall thermostat can only display the adjusted target. If the wall temperature is changed by hand, this will override any adjustments made by this program for a set period of time before the schedule resumes.
 
 ## Current limitations
 - Only one air conditioner is displayed, the API chooses the first device.  I only have one system, so feel free to contribute changes if you have more than one A/C unit.
 - Timeout on token is not enabled, when token is invalid the API will re-authenticate.
-- Auth. Token is not cached (Future Release)
-- Previous thermostat state is not cached (Future Release)
+- Auth. Token is not cached
+- Previous thermostat state is not cached
+- For US users, Aqara devices seem like the obvious sensor choice. If anyone knows how to easily read these (no ferreting around for tokens in encypted backups) please let me know as I've had no luck :-(
+
+## Wouldn't it be nice ...
+- If HomeKit devices could be allowed read other HomeKit devices state (e.g. sensors). I realize there are security concerns here, but still ...
+
+## TODO
+- **Lots and lots of testing. Seriously this could burn your house down at the moment.**
+- Web UI to setup the schedule in a way which doesn't involve ending JSON.
 
 ## Contributions
 Portions of this software adapted from the projects listed below.  A huge thank you, for all their work.
+
+- The original homebridge-fujitsu plugin https://github.com/smithersDBQ/homebridge-fujitsu
 
 - The pyfujitsu project under Apache License
 Copyright (c) 2018 Mmodarre https://github.com/Mmodarre/pyfujitsu
 
 - The homebridge-dummy-thermostat under the Unlicense
 https://github.com/X1ZOR/homebridge-dummy-thermostat
+
+- The node-mihome library https://github.com/maxinminax/node-mihome
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
     HbAPI = homebridge;
-    homebridge.registerAccessory("homebridge-fujitsu", "FGLairThermostat", Thermostat);
+    homebridge.registerAccessory("homebridge-fujitsu-smart", "FGLairSmartThermostat", Thermostat);
 };
 
 const OPERATION_MODE = { "off":0, "auto":2, "cool":3, "dry":4, "fan_only":5, "heat":6, 0:"off", 2:"auto", 3:"cool", 4:"dry", 5:"fan_only", 6:"heat"}
@@ -35,6 +35,7 @@ function Thermostat(log, config) {
     this.password = config.password || '';
     this.temperatureDisplayUnits = config.temperatureDisplayUnits || 0;
     this.targetTemperature = 20;
+    this.adjustedTargetTemperature = this.targetTemperature;
     this.keyTargetTemperature = 0;
     this.currentTemperature = 20;
 
@@ -42,16 +43,18 @@ function Thermostat(log, config) {
     this.keyCurrentHeatingCoolingState = 0;
     this.currentHeatingCoolingState = 6;
 
+    this.fanSpeed = 0;
+    this.keyFanSpeed = 0;
+
     this.deviceProperties = [];
 
     this.log(this.name);
     this.service = new Service.Thermostat(this.name);
+    this.fan = new Service.Fanv2(`${this.name} Fan`);
     this.api = require('./fglairAPI.js')
     this.api.setLog(this.log);
     this.api.setToken(this.token);
     this.api.setRegion(this.region);
-
-    that = this;
 
     this.api.getAuth(this.userName ,this.password, (err, token) =>
     {
@@ -65,14 +68,20 @@ function Thermostat(log, config) {
             }
             else
             {
-
                 this.serial = data[0]; //Only one thermostat is supported
-                this.updateAll(that);
-                setInterval( this.updateAll, this.interval, that );
+                this.updateAll(this);
+                setInterval( this.updateAll, this.interval, this );
             }
         });
 
     });
+
+    // Enable MIIO sensors (if configured)
+    this.pauseProgramUntil = 0;
+    if (config.smart) {
+        this.miio = require('./miio');
+        this.miio.start(config.smart);
+    }
 }
 
 Thermostat.prototype.updateAll = function(ctx)
@@ -89,8 +98,48 @@ Thermostat.prototype.updateAll = function(ctx)
                 //this.log(prop['property']['name']);
                 if( prop['property']['name'] == 'adjust_temperature' )
                 {
-                    ctx.targetTemperature = parseInt(prop['property']['value'])/10
-                    ctx.currentTemperature = ctx.targetTemperature;
+                    const originalTarget = parseInt(prop['property']['value']) / 10;
+                    let adjustedTarget = originalTarget;
+                    let target;
+
+                    // HVAC off, so adjust nothing.
+                    if (ctx.currentHeatingCoolingState === Characteristic.CurrentHeatingCoolingState.OFF) {
+                        target = adjustedTarget;
+                    }
+                    // Temp changed externally. Either this was by a manual user change
+                    // or by a program change. We don't know which but we want to respect the user
+                    // change so we make the target the adjusted too (ie. no diff).
+                    // This will persist until the program changes.
+                    else if (ctx.adjustedTargetTemperature != adjustedTarget) {
+                        target = adjustedTarget;
+                        if (ctx.miio) {
+                            ctx.pauseProgramUntil = Date.now() + ctx.miio.holdTime;
+                        }
+                    }
+                    else {
+                        // No external change, so target is whatever it currenty is.
+                        target = ctx.targetTemperature;
+                    }
+
+                    if (ctx.miio) {
+                        ctx.currentTemperature = ctx.miio.currentTempC;
+                        if (Date.now() > ctx.pauseProgramUntil) {
+                            // Program is different from the program when we last stopped applying
+                            // differences, so we can do that again now.
+                            adjustedTarget = target - ctx.miio.currentTempDiffC;
+                        }
+                    }
+                    else {
+                        ctx.currentTemperature = target;
+                    }
+
+                    // If adjustedTarget is now different, update thermostat
+                    if (originalTarget !== adjustedTarget) {
+                        ctx.api.setDeviceProp(this.keyTargetTemperature, Math.round(adjustedTarget * 10), () => {});
+                    }
+                    ctx.targetTemperature = target;
+                    ctx.adjustedTargetTemperature = adjustedTarget;
+
                     //ctx.log("[" + ctx.serial + "] Got Temperature: "+ ctx.targetTemperature + ":" + ctx.currentTemperature);
                     ctx.service.updateCharacteristic(Characteristic.TargetTemperature, ctx.targetTemperature);
                     ctx.service.updateCharacteristic(Characteristic.CurrentTemperature, ctx.currentTemperature);
@@ -101,9 +150,6 @@ Thermostat.prototype.updateAll = function(ctx)
                     let mode = OPERATION_MODE[prop['property']['value']];
                     switch(mode)
                     {
-                        case "off":
-                            ctx.targetHeatingCoolingState = Characteristic.TargetHeatingCoolingState.OFF;
-                            break;
                         case "auto":
                             ctx.targetHeatingCoolingState = Characteristic.TargetHeatingCoolingState.AUTO;
                             break;
@@ -111,10 +157,11 @@ Thermostat.prototype.updateAll = function(ctx)
                             ctx.targetHeatingCoolingState = Characteristic.TargetHeatingCoolingState.HEAT;
                             break;
                         case "cool":
-                        case "fan":
+                        case "fan_only":
                         case "dry":
                             ctx.targetHeatingCoolingState = Characteristic.TargetHeatingCoolingState.COOL;
                             break;
+                        case "off":
                         default:
                             ctx.targetHeatingCoolingState = Characteristic.TargetHeatingCoolingState.OFF;
                             break;
@@ -122,16 +169,14 @@ Thermostat.prototype.updateAll = function(ctx)
 
                     switch(mode)
                     {
-                        case "off":
-                            ctx.currentHeatingCoolingState = Characteristic.CurrentHeatingCoolingState.OFF;
-                            break;
                         case "heat":
                             ctx.currentHeatingCoolingState = Characteristic.CurrentHeatingCoolingState.HEAT;
                             break;
                         case "cool":
-                        case "fan":
+                        case "fan_only":
                             ctx.currentHeatingCoolingState = Characteristic.CurrentHeatingCoolingState.COOL;
                             break;
+                        case "off":
                         default:
                             ctx.currentHeatingCoolingState = Characteristic.CurrentHeatingCoolingState.OFF;
                             break;
@@ -142,7 +187,20 @@ Thermostat.prototype.updateAll = function(ctx)
                     //ctx.log("[" + ctx.serial + "] Got HeatingCooling State: "+ ctx.targetHeatingCoolingState);
                     ctx.service.updateCharacteristic(Characteristic.CurrentHeatingCoolingState, ctx.currentHeatingCoolingState);
                     ctx.service.updateCharacteristic(Characteristic.TargetHeatingCoolingState, ctx.targetHeatingCoolingState);
+                    ctx.fan.updateCharacteristic(Characteristic.Active, ctx.currentHeatingCoolingState === Characteristic.CurrentHeatingCoolingState.OFF ? 0 : 1);
+                }
+                else if( prop['property']['name'] == 'fan_speed' )
+                {
+                    ctx.fanSpeed = parseInt(prop['property']['value']);
+                    ctx.fan.updateCharacteristic(Characteristic.RotationSpeed,
+                        ctx.fanSpeed === 0 ? 10 :
+                        ctx.fanSpeed === 1 ? 30 :
+                        ctx.fanSpeed === 2 ? 60 :
+                        100
+                    );
+                    ctx.fan.updateCharacteristic(Characteristic.TargetFanState, ctx.fanSpeed === 4 ? 1 : 0);
 
+                    this.keyFanSpeed = prop['property']['key'];
                 }
             }); //end of foreach
             ctx.log("[" + ctx.serial + "] temp: " + ctx.targetTemperature + "C, mode: " + HK_MODE[ctx.targetHeatingCoolingState]);
@@ -160,7 +218,6 @@ Thermostat.prototype.getTargetHeatingCoolingState = function(cb) {
 };
 
 Thermostat.prototype.setTargetHeatingCoolingState = function(val, cb) {
-
 	let fgl_val = OPERATION_MODE[HK_MODE[val]];
 	this.log("Setting Target Mode to " + fgl_val + ":" +HK_MODE[val]);
     this.service.updateCharacteristic(Characteristic.TargetHeatingCoolingState, val);
@@ -182,14 +239,19 @@ Thermostat.prototype.getTargetTemperature = function(cb) {
 };
 
 Thermostat.prototype.setTargetTemperature = function(val, cb) {
-	this.log("Setting Temperature to " + val);
-    this.api.setDeviceProp(this.keyTargetTemperature, Math.round(val) * 10, (err) =>
+    this.log("Setting Temperature to " + val);
+    this.targetTemperature = val;
+    this.adjustedTargetTemperature = val;
+    this.pauseProgramUntil = 0;
+    if (this.miio)
     {
-    	this.service.updateCharacteristic(Characteristic.TargetTemperature, Math.round(val));
+        this.adjustedTargetTemperature -= this.miio.currentTempDiffC;
+    }
+    this.api.setDeviceProp(this.keyTargetTemperature, Math.round(this.adjustedTargetTemperature * 10), (err) =>
+    {
+        this.service.updateCharacteristic(Characteristic.TargetTemperature, this.targetTemperature);
         cb(err);
     });
-
-
 };
 
 Thermostat.prototype.getTemperatureDisplayUnits = function(cb) {
@@ -205,6 +267,54 @@ Thermostat.prototype.setTemperatureDisplayUnits = function(val, cb) {
 Thermostat.prototype.getName = function(cb) {
     cb(null, this.name);
 };
+
+Thermostat.prototype.setFanActive = function(val, cb) {
+    this.log('setFanActive', val);
+    // ... update ...
+    cb();
+}
+
+Thermostat.prototype.setTargetFanState = function(val, cb) {
+    this.log('setTargetFanState', val ? 'automatic' : 'manual');
+    if( val === 0)
+    {
+        // Manual
+        this.fan.getCharacteristic(Characteristic.RotationSpeed).getValue((err, value) =>
+        {
+            if( err)
+            {
+                cb(err);
+            }
+            else
+            {
+                this.setRotationSpeed(value, cb);
+            }
+        });
+    }
+    else
+    {
+        // Automatic
+        this.fanSpeed = 4;
+        this.api.setDeviceProp(this.keyFanSpeed, this.fanSpeed, cb);
+    }
+}
+
+Thermostat.prototype.setRotationSpeed = function(val, cb) {
+    this.log('setRotationSpeed', val);
+    if (val <= 10) {
+        this.fanSpeed = 0;
+    }
+    else if (val <= 30) {
+        this.fanSpeed = 1;
+    }
+    else if (val <= 60) {
+        this.fanSpeed = 2;
+    }
+    else {
+        this.fanSpeed = 3;
+    }
+    this.api.setDeviceProp(this.keyFanSpeed, this.fanSpeed, cb);
+}
 
 Thermostat.prototype.getServices = function () {
     this.informationService = new Service.AccessoryInformation();
@@ -239,5 +349,24 @@ Thermostat.prototype.getServices = function () {
         .getCharacteristic(Characteristic.Name)
         .on('get', this.getName.bind(this));
 
-	return [this.informationService, this.service];
+    this.fan
+        .getCharacteristic(Characteristic.Active)
+        .on('set', this.setFanActive.bind(this));
+
+    this.fan
+        .getCharacteristic(Characteristic.TargetFanState)
+        .on('set', this.setTargetFanState.bind(this));
+
+    this.fan
+        .getCharacteristic(Characteristic.RotationSpeed)
+        .on('set', this.setRotationSpeed.bind(this));
+
+    this.service.isPrimaryService = true;
+    this.service.linkedServices = [ this.fan ];
+
+	return [this.informationService, this.service, this.fan];
 };
+
+// TargetFanState = Manual 0, Automatic 1
+// CurrentFanState = Inactive 0, Idle 1, Active 2
+// RotationSpeed = 0-100
