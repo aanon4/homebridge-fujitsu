ea891fcaefa15088a48414342c56f5be3b73e158
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-10-28 14:59:56 -0700

    Move multiple schedules into smart

--- smart/index.js
+++ smart/index.js
@@ -1,9 +1,15 @@
+const FS = require('fs');
+const Path = require('path');
 const Bus = require('./bus');
 const Feels = require('feels');
 
 const MODE_OFF = 0;
 const MODE_COOL = 2;
 const MODE_HEAT = 1;
+
+const AWAY_VALID = [
+  [ 6 * 60, 21 * 60 ] // 6am - 9pm
+];
 
 class Smart {
 
@@ -15,6 +21,8 @@ class Smart {
     this.feelsLike = false;
     this.holdTime = 0;
     this.unit = 'c';
+    this.autoAway = false;
+    this.away = false
     this.currentProgram = {
       targetMode: MODE_OFF,
       targetTemperature: null,
@@ -29,18 +37,37 @@ class Smart {
   async start(config, log, hbapi) {
     this.log = log;
     this.hbapi = hbapi;
+    this.stateFile = Path.join(hbapi.user.persistPath(), 'smart-state.json');
     this.referenceDevice = config.reference;
     this.feelsLike = config.feelslike || false;
     this.holdTime = (config.hold || 60) * 60 * 1000;
     this.unit = (config.unit || 'c').toLowerCase();
     this.currentProgram.pause = 0;
+    this.autoAway = config.autoaway || false;
 
-    await this._miio(config);
-    this._web(config);
+    if (config.miio) {
+      const miio = require('./sensors/miio');
+      await miio.login(config.miio, this.log);
+      this.sensors = miio;
+    }
 
-    this.setSchedule(this.buildSchedule(config.schedule));
+    if (config.schedule) {
+      this.setSchedule('normal', this.buildSchedule(config.schedule));
+      this.setScheduleTo('normal');
+    }
+    else {
+      this.loadState();
+      this.web = require('./web/server');
+      this.web.start(this, config, this.hbapi, this.log);
+    }
 
     const poll = () => {
+      if (this.autoAway) {
+        this._checkAway();
+        if (this.away && this.selectedSchedule !== 'away') {
+          this.setScheduleTo('away');
+        }
+      }
       this._updateSensors().then(() => {
         this._updateProgram();
         this.poller = setTimeout(poll, Math.max(0, 60000 - Date.now() % 60000));
@@ -57,22 +84,12 @@ class Smart {
   stop() {
     if (this.poller) {
       clearTimeout(this.poller);
-      this.poller = null;
     }
-  }
-
-  async _miio(config) {
-    if (config.miio) {
-      const miio = require('./sensors/miio');
-      await miio.login(config.miio, this.log);
-      this.sensors = miio;
+    if (this.weather) {
+      this.weather.stop();
     }
-  }
-
-  _web(config) {
-    if (!config.schedule) {
-      this.web = require('./web/server');
-      this.web.start(this, config, this.hbapi, this.log);
+    if (this.web) {
+      this.web.stop();
     }
   }
 
@@ -172,16 +189,17 @@ class Smart {
     const now = new Date();
     const weektime = (now.getDay() * 24 + now.getHours()) * 60 + now.getMinutes();
 
-    if (!this.schedule.length) {
+    const schedule = this.schedules[this.selectedSchedule];
+    if (!schedule.length) {
       return null;
     }
 
     // Find the approximate schedule for the current time.
     let start = 0;
-    let end = this.schedule.length;
+    let end = schedule.length;
     while (start < end) {
       const i = Math.floor((start + end) / 2);
-      if (this.schedule[i].weektime <= weektime) {
+      if (schedule[i].weektime <= weektime) {
         start = i + 1;
       }
       else {
@@ -190,9 +208,9 @@ class Smart {
     }
 
     // Walk backwards from this point to find the exact match based on looping around the schedule list and handle triggers
-    let pos = start - 1;
+    let pos = (schedule.length + start - 1) % schedule.length;
     for (;;) {
-      const sched = this.schedule[pos];
+      const sched = schedule[pos];
       if (weektime >= sched.weektime) {
         if (sched.trigger) {
           // Clear trigger which is > 24 hours old
@@ -215,7 +233,7 @@ class Smart {
           return sched;
         }
       }
-      pos = (this.schedule.length + pos - 1) % this.schedule.length;
+      pos = (schedule.length + pos - 1) % schedule.length;
       if (pos === start) {
         // No schedule to run
         this.log.debug('_getSchedule: program: none');
@@ -224,12 +242,50 @@ class Smart {
     }
   }
 
-  setSchedule(schedule) {
+  _checkAway() {
+    let motion = false;
+    for (let name in this.devices) {
+      const device = this.devices[name];
+      if (device.motion && device.motion.online && device.motion.motion1800) {
+        motion = true;
+        break;
+      }
+    }
+
+    if (motion) {
+      this.away = null;
+    }
+    else if (!this.away) {
+      const now = new Date();
+      const daytime = now.getHours() * 60 + now.getMinutes();
+      if (AWAY_VALID.find(period => period[0] <= daytime && period[1] >= daytime)) {
+        this.away = Date.now();
+      }
+    }
+  }
+
+  setSchedule(name, schedule) {
+    this.log.debug('setSchedule:', name, schedule);
     const copy = [].concat(schedule);
     copy.sort((a, b) => a.weektime - b.weektime + (a.trigger ? 0.5 : 0) - (b.trigger ? 0.5 : 0));
-    this.schedule = copy;
-    Bus.emit('smart.schedule.update', this.schedule);
+    this.schedules[name] = copy;
+    this.saveState();
+    Bus.emit('smart.schedule.update', name, copy);
     this._updateProgram();
+  }
+
+  getSchedule(name) {
+    return this.schedules[name];
+  }
+
+  setScheduleTo(name) {
+    this.log.debug('setScheduleTo:', name);
+    if (this.selectedSchedule !== name && name in this.schedules) {
+      this.selectedSchedule = name;
+      this.saveState();
+      Bus.emit('smart.schedule.update', this.selectedSchedule, this.schedules[this.selectedSchedule]);
+      this._updateProgram();
+    }
   }
 
   buildSchedule(schedule) {
@@ -288,6 +344,7 @@ class Smart {
             weektime: day * 24 * 60 + time,
             low: low,
             high: high,
+            trigger: sched.tigger,
             rooms: sched.rooms
           });
         });
@@ -303,6 +360,49 @@ class Smart {
 
   getDevices() {
     return this.devices;
+  }
+
+  loadState() {
+    try {
+      const info = JSON.parse(FS.readFileSync(this.stateFile, { encoding: 'utf8' }));
+      this.selectedSchedule = info.schedule.selected;
+      this.schedules = info.schedule.schedules;
+    }
+    catch (_) {
+      this.selectedSchedule = 'normal';
+      this.schedules = {
+        'normal': [],
+        'vacation': [],
+        'away': []
+      };
+    }
+  }
+
+  saveState() {
+    const json = JSON.stringify({
+      version: 1,
+      schedule: {
+        selected: this.selectedSchedule,
+        schedules: this.schedules
+      }
+    });
+    FS.readFile(this.stateFile, { encoding: 'utf8' }, (e, info) => {
+      if (!e) {
+        if (info == json) {
+          return;
+        }
+        FS.writeFile(`${this.stateFile}.bak`, info, { encoding: 'utf8' }, e => {
+          if (e) {
+            this.log.error('saveState: copy:', e);
+          }
+        });
+      }
+      FS.writeFile(this.stateFile, json, { encoding: 'utf8' }, e => {
+        if (e) {
+          this.log.error('saveState:', e);
+        }
+      });
+    });
   }
 }
 
--- smart/web/main.js
+++ smart/web/main.js
@@ -1,5 +1,3 @@
-const FS = require('fs');
-const Path = require('path');
 const Base = require('./base');
 const Template = require('./template');
 const Bus = require('../bus');
@@ -13,72 +11,11 @@ class Main extends Base {
     this._weatherUpdate = this._weatherUpdate.bind(this);
 
     this.smart = smart;
-    this.scheduleFile = Path.join(hap.user.persistPath(), 'smart-schedule.json');
-
-    try {
-      const info = JSON.parse(FS.readFileSync(this.scheduleFile, { encoding: 'utf8' }));
-      const make = (schedule) => {
-        return schedule.map(sched => {
-          return {
-            title: sched.title,
-            sliders: sched.sliders.map(slider => {
-              return {
-                high: this.toU(slider.high),
-                low: this.toU(slider.low),
-                time: slider.time,
-                trigger: slider.trigger,
-                rooms: slider.rooms
-              };
-            })
-          };
-        });
-      }
-      this.state.selected = info.selected;
-      this.state.schedules = {
-        normal: make(info.normal),
-        vacation: make(info.vacation),
-        away: make(info.away)
-      };
-      this.state.schedule = this.state.schedules[this.state.selected];
-    }
-    catch (_) {
-      this.createDefaultSliders();
-    }
-    this.smart.setSchedule(this.generateSchedule());
-    this.state.rooms = [];
-    this.state.feelsLike = this.smart.feelsLike;
-  }
-
-  toU(v) {
-    return this.smart.unit === 'f' ? Math.round(v / 5 * 9 + 32) : Math.round(v * 2) / 2;
-  }
-
-  toC(v) {
-    return this.smart.unit === 'f' ? (v - 32) / 9 * 5 : v;
   }
 
   main(ctx) {
     this.updateState();
     super.main(ctx);
-  }
-
-  createDefaultSliders() {
-    const make = () => {
-      return [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ].map(title => {
-        const day = { title: title, sliders: [] };
-        for (let i = 0; i < 8; i++) {
-          day.sliders.push({ low: this.toU(10), high: this.toU(25), time: '', trigger: null, rooms: {} });
-        }
-        return day;
-      });
-    }
-    this.state.selected = 'normal';
-    this.state.schedules = {
-      normal: make(),
-      vacation: make(),
-      away: make()
-    };
-    this.state.schedule = this.state.schedules[this.state.selected];
   }
 
   watch() {
@@ -94,8 +31,13 @@ class Main extends Base {
   }
 
   _programUpdate() {
+    const oldSchedule = this.state.selected;
     this.updateState();
     this.html('thermostat', Template.thermostat(this.state));
+    this.html('menu', Template.menu(this.state));
+    if (this.state.selected !== oldSchedule) {
+      this.html('schedule', Template.schedule(this.state));
+    }
   }
 
   _deviceUpdate() {
@@ -111,8 +53,21 @@ class Main extends Base {
     this.html('weather', Template.weather(this.state));
   }
 
+  toU(v) {
+    return this.smart.unit === 'f' ? Math.round(v / 5 * 9 + 32) : Math.round(v * 2) / 2;
+  }
+
+  toC(v) {
+    return this.smart.unit === 'f' ? (v - 32) / 9 * 5 : v;
+  }
+
   updateState() {
+    if (this.state.selected !== this.smart.selectedSchedule) {
+      this.state.selected = this.smart.selectedSchedule;
+      this.state.schedule = this._smart2visual(this.smart.getSchedule(this.state.selected));
+    }
     this.state.rooms = [];
+    this.state.feelsLike = this.smart.feelsLike;
     const devices = this.smart.getDevices();
     for (let name in devices) {
       const device = devices[name];
@@ -151,8 +106,7 @@ class Main extends Base {
         slider.time = msg.time;
         slider.trigger = msg.trigger;
         slider.rooms = msg.rooms;
-        this.saveState();
-        this.smart.setSchedule(this.generateSchedule());
+        this.smart.setSchedule(this.state.selected, this._visual2smart(this.state.schedule));
       }
     }
   }
@@ -163,12 +117,7 @@ class Main extends Base {
       case 'vacation':
       case 'away':
         if (msg.schedule !== this.state.selected) {
-          this.state.selected = msg.schedule;
-          this.state.schedule = this.state.schedules[this.state.selected];
-          this.html('menu', Template.menu(this.state));
-          this.html('schedule', Template.schedule(this.state));
-          this.smart.setSchedule(this.generateSchedule());
-          this.saveState();
+          this.smart.setScheduleTo(msg.schedule);
         }
         break;
       default:
@@ -176,52 +125,59 @@ class Main extends Base {
     }
   }
 
-  saveState() {
-    const unmake = (schedule) => {
-      return schedule.map(sched => {
-        return {
-          title: sched.title,
-          sliders: sched.sliders.map(slider => {
-            return {
-              high: this.toC(slider.high),
-              low: this.toC(slider.low),
-              time: slider.time,
-              trigger: slider.trigger,
-              rooms: slider.rooms
-            };
-          })
-        };
-      });
-    }
-    const json = JSON.stringify({
-      selected: this.state.selected,
-      normal: unmake(this.state.schedules.normal),
-      vacation: unmake(this.state.schedules.vacation),
-      away: unmake(this.state.schedules.away)
-    });
-    FS.readFile(this.scheduleFile, { encoding: 'utf8' }, (e, info) => {
-      if (!e) {
-        if (info == json) {
-          return;
-        }
-        FS.writeFile(`${this.scheduleFile}.bak`, info, { encoding: 'utf8' }, e => {
-          if (e) {
-            console.error('saveState: copy:', e);
-          }
-        });
+  _smart2visual(schedule) {
+    function toT(wt) {
+      const t = wt % (24 * 60);
+      const h = Math.floor(t / 60);
+      const m = `0${t % 60}`.substr(-2);
+      if (h === 0) {
+        return `12:${m}am`;
       }
-      FS.writeFile(this.scheduleFile, json, { encoding: 'utf8' }, e => {
-        if (e) {
-          console.error('saveState:', e);
-        }
+      if (h < 12) {
+        return `${h}:${m}am`;
+      }
+      if (h == 12) {
+        return `12:${m}pm`;
+      }
+      return `${h}:${m}pm`;
+    }
+    const days = [
+      { title: 'Sunday', sliders:[] },
+      { title: 'Monday', sliders:[] },
+      { title: 'Tuesday', sliders:[] },
+      { title: 'Wednesday', sliders:[] },
+      { title: 'Thursday', sliders:[] },
+      { title: 'Friday', sliders:[] },
+      { title: 'Saturday', sliders:[] }
+    ];
+    schedule.forEach(sched => {
+      const day = days[Math.floor(sched.weektime / (24 * 60))];
+      day.sliders.push({
+        time: toT(sched.weektime),
+        low: this.toU(sched.low),
+        high: this.toU(sched.high),
+        tigger: sched.trigger && sched.trigger[0] && sched.trigger[0].room,
+        rooms: Object.keys(sched.rooms).reduce((rooms, room) => {
+          rooms[room] = {
+            always: !!(rooms[room].empty && rooms[room].occupied),
+            occupied: !rooms[room].empty
+          }
+          return rooms;
+        }, {})
       });
     });
+    days.forEach(day => {
+      while (day.sliders.length < 8) {
+        day.sliders.push({ low: this.toU(10), high: this.toU(25), time: '', trigger: null, rooms: {} });
+      }
+    });
+    return days;
   }
 
-  generateSchedule() {
+  _visual2smart(schedule) {
     const sched = [];
-    for (let day = 0; day < this.state.schedule.length; day++) {
-      const sliders = this.state.schedule[day].sliders;
+    for (let day = 0; day < schedule.length; day++) {
+      const sliders = schedule[day].sliders;
       for (let i = 0; i < sliders.length; i++) {
         const slider = sliders[i];
         if (slider.time) {
